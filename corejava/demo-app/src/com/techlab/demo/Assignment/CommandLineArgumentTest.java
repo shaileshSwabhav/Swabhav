249e0b3019eff9544438fee02325e773fbe4cdca
shaileshhb <shaileshb.0720@gmail.com>
2020-04-22 11:28:02 +0530

    CommandLineArgumentTest completed

---
+++ corejava/demo-app/src/com/techlab/demo/Assignment/CommandLineArgumentTest.java
@@ -0,0 +1,15 @@
+package com.techlab.demo.Assignment;
+
+public class CommandLineArgumentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		System.out.println("Sum of " + a + " and " + b + " is " + (a+b));
+		
+	}
+
+}
