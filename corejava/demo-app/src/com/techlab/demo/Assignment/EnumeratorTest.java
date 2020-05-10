143c649fd0dfa87425da09f9efbbf11ee906563e
shaileshhb <shaileshb.0720@gmail.com>
2020-05-10 13:58:51 +0530

    day 8 assignment

---
+++ corejava/demo-app/src/com/techlab/demo/Assignment/EnumeratorTest.java
@@ -0,0 +1,18 @@
+package com.techlab.demo.Assignment;
+
+
+enum DaysOfWeek
+{ sunday, monday, tuesday, wednesday, thrusday, friday, saturday}
+
+public class EnumeratorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DaysOfWeek d;
+		d = DaysOfWeek.saturday;
+		System.out.println("Day is: " + d);
+		
+	}
+
+}
