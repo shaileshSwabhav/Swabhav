efced063e339f03b96af18eb043d5c2d2db044a5
shaileshhb <shaileshb.0720@gmail.com>
2020-05-23 16:16:58 +0530

    day12 assignment

--- corejava/demo-app/src/com/techlab/demo/Assignment/custom-annotations-demo-app/src/com/techlab/custom/annotation/test/CustomAnnotationTest.java
+++ corejava/demo-app/src/com/techlab/demo/Assignment/custom-annotations-demo-app/src/com/techlab/custom/annotation/test/CustomAnnotationTest.java
@@ -13,40 +13,20 @@ public class CustomAnnotationTest {
 		CustomAnnotation c = new CustomAnnotation();
 
 		printNumberOfAnnotation(c);
-
-		checkCases(c);
+//		checkCases(c);
 	}
 
-	public static void printNumberOfAnnotation(CustomAnnotation c) {
+	public static void printNumberOfAnnotation(CustomAnnotation c) throws Exception {
 
 		int countAnnotation = 0;
+		int countPassedCases = 0, countFailedCases = 0;
+		boolean isTrue = true;
 		Method[] method = c.getClass().getMethods();
 		for (Method m : method) {
 
 			Annotation[] annot = m.getDeclaredAnnotations();
 			for (Annotation a : annot) {
 				countAnnotation++;
-			}
-		}
-		System.out.println("No. of annotations: " + countAnnotation + "\n");
-	}
-
-	public static void checkCases(CustomAnnotation c) throws Exception {
-
-		int countPassedCases = 0, countFailedCases = 0;
-		boolean isTrue = true;
-		
-		Method[] method = c.getClass().getDeclaredMethods();
-		/*
-		 * for (Method m : method) { m.setAccessible(true); boolean value = (boolean)
-		 * m.invoke(c, null); if (value == checkValue) { countCases++;
-		 * System.out.println(m.getName()); } }
-		 */
-		
-		for (Method m : method) {
-
-			Annotation[] annot = m.getDeclaredAnnotations();
-			for (Annotation a : annot) {
 
 				m.setAccessible(true);
 				boolean value = (boolean) m.invoke(c, null);
@@ -56,10 +36,41 @@ public class CustomAnnotationTest {
 				} else {
 					countFailedCases++;
 				}
+
 			}
 		}
-		
+		System.out.println("No. of annotations: " + countAnnotation);
 		System.out.println("Number of Passed Cases: " + countPassedCases);
 		System.out.println("Number of Failed Cases: " + countFailedCases);
 	}
+
+	/*
+	public static void checkCases(CustomAnnotation c) throws Exception {
+
+		int countPassedCases = 0, countFailedCases = 0;
+		boolean isTrue = true;
+
+		Method[] method = c.getClass().getDeclaredMethods();
+
+		for (Method m : method) {
+			m.setAccessible(true);
+			boolean value = (boolean) m.invoke(c, null);
+			if (value == isTrue) {
+				
+				System.out.println(m.getName());
+			}
+		}
+
+		/*
+		 * for (Method m : method) {
+		 * 
+		 * Annotation[] annot = m.getDeclaredAnnotations(); for (Annotation a : annot) {
+		 * 
+		 * m.setAccessible(true); boolean value = (boolean) m.invoke(c, null); if (value
+		 * == isTrue) { countPassedCases++; // System.out.println(m.getName()); } else {
+		 * countFailedCases++; } } }
+		 
+
+	}
+	*/
 }
