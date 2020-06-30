b6ce9488db54bcdad8ab767fbd89ef611690488b
shaileshhb <shaileshb.0720@gmail.com>
2020-06-30 13:49:17 +0530

    day22 assignment

---
+++ OOAD/Assignment/solid-app/src/com/techlab/dipviolation/DVD.java
@@ -0,0 +1,17 @@
+package com.techlab.dipviolation;
+
+public class DVD {
+
+	public void seeReview() {
+		System.out.println("See DVD Reviews");
+	}
+
+	public void watchSample() {
+		System.out.println("Watching DVD sample");
+	}
+}
+
+// now we need to add this to the shelf
+// we cant do that bcoz it will violate OCP has we will end up modifying shelf class
+// so we need to have a abstract(interface) class 
+// which we help us to add book and DVD to the shelf
