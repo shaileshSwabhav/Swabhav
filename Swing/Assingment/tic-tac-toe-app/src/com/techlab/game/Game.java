a69ad8f026cbea015324b26f99c630f7b894d49a
shaileshhb <shaileshb.0720@gmail.com>
2020-07-13 13:47:20 +0530

    day26 assignment

--- Swing/Assingment/tic-tac-toe-app/src/com/techlab/game/Game.java
+++ Swing/Assingment/tic-tac-toe-app/src/com/techlab/game/Game.java
@@ -13,7 +13,7 @@ public class Game extends JFrame implements ActionListener {
 	private JFrame frame;
 	private JPanel panel;
 	private JLabel player1, player2, player, instructions1, instructions2, instructions3, boardSize;
-	private JTextField t1, t2,  t3;
+	private JTextField t1, t2, t3;
 	private JButton submit;
 	private JButton[] playBoard;
 	private Board board;
@@ -40,11 +40,10 @@ public class Game extends JFrame implements ActionListener {
 		player1.setBounds(20, 100, 200, 20);
 		player2.setBounds(20, 140, 150, 20);
 		boardSize.setBounds(20, 180, 100, 20);
-		
+
 		player1.setForeground(Color.blue);
 		player2.setForeground(Color.blue);
 		boardSize.setForeground(Color.blue);
-
 
 		t1.setBounds(145, 100, 220, 20);
 		t2.setBounds(145, 140, 220, 20);
@@ -72,7 +71,7 @@ public class Game extends JFrame implements ActionListener {
 				players.add(new Player(player1Name, Mark.X));
 				players.add(new Player(player2Name, Mark.O));
 				board.setBoardSize(boardSize);
-				
+
 				play();
 				dispose();
 			}
@@ -89,77 +88,96 @@ public class Game extends JFrame implements ActionListener {
 
 		frame = new JFrame("Tic-Tac-Toe");
 		panel = new JPanel();
-		
+
 		player = new JLabel();
 		instructions1 = new JLabel();
 		instructions2 = new JLabel();
 		instructions3 = new JLabel();
-				
+
 		frame.setLayout(null);
-		
+
 		board.gameBoard();
 		playBoard = board.getBoard();
-		
+
 		for (int i = 0; i < playBoard.length; i++) {
 
 			panel.add(playBoard[i]);
 			playBoard[i].addActionListener(this);
 
 		}
-		
+
 		String inst1 = "Welcome To TIC-TAC-TOE.";
-		String inst2 =	getCurrentPlayer().getPlayerName() + " has mark " + getCurrentPlayer().getPlayerMark(); 
+		String inst2 = getCurrentPlayer().getPlayerName() + " has mark " + getCurrentPlayer().getPlayerMark();
 		changePlayer();
 		String inst3 = getCurrentPlayer().getPlayerName() + " has mark " + getCurrentPlayer().getPlayerMark();
 		changePlayer();
-		
+
 		instructions1.setText(inst1);
 		instructions2.setText(inst2);
 		instructions3.setText(inst3);
-		
-		instructions1.setBounds(15, 5, 400, 40);		
+
+		instructions1.setBounds(15, 5, 400, 40);
 		instructions2.setBounds(15, 20, 400, 40);
 		instructions3.setBounds(15, 35, 400, 40);
-		
+
 		instructions1.setForeground(Color.blue);
 		instructions2.setForeground(Color.blue);
 		instructions3.setForeground(Color.blue);
 
-
 		player.setText("It's " + getCurrentPlayer().getPlayerName() + "'s turn");
 		player.setForeground(Color.red);
 		player.setBounds(15, 50, 100, 40);
-		
+
 		frame.add(instructions1, JLabel.CENTER);
 		frame.add(instructions2, JLabel.CENTER);
 		frame.add(instructions3, JLabel.CENTER);
 		frame.add(player, JLabel.CENTER);
-		
+
 		panel.setLayout(new GridLayout(board.getBoardSize(), board.getBoardSize()));
 		panel.setBounds(40, 100, 250, 250);
-		
+
 		frame.add(panel);
 		frame.setSize(400, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-		board.addMarkToCell(getCurrentPlayer().getPlayerMark(), (JButton)e.getSource());
+
+		board.addMarkToCell(getCurrentPlayer().getPlayerMark(), (JButton) e.getSource());
 		if (getStatus().equals("WIN")) {
-			JOptionPane.showMessageDialog(null, getCurrentPlayer().getPlayerName() + "is a WINNER!!!!!");
-			frame.dispose();
+
+			int output = JOptionPane.showConfirmDialog(frame,
+					getCurrentPlayer().getPlayerName() + " is WINNER!!\nDo you want to continue?", "Game Over",
+					JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+			if (output == JOptionPane.YES_OPTION) {
+				frame.dispose();
+				changePlayer();
+				play();				
+			} else if (output == JOptionPane.NO_OPTION) {
+				frame.dispose();
+			}
+
 		} else if (getStatus().equals("DRAW")) {
-			JOptionPane.showMessageDialog(null, "Its a draw....");
-			frame.dispose();
+
+			int output = JOptionPane.showConfirmDialog(frame,
+					"It's a DRAW...\nDo you want to continue?", "Game Over",
+					JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+			if (output == JOptionPane.YES_OPTION) {
+				frame.dispose();
+				changePlayer();
+				play();
+			} else if (output == JOptionPane.NO_OPTION) {
+				frame.dispose();
+			}
+
 		}
-		
+
 		changePlayer();
-		
+
 		player.setText("It's " + getCurrentPlayer().getPlayerName() + " turn");
 		player.setBounds(15, 50, 100, 40);
 		player.setForeground(Color.red);
