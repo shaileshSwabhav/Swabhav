9b992533063d524f2975aa150fdf912a54c7f14a
shaileshhb <shaileshb.0720@gmail.com>
2020-08-17 18:14:54 +0530

    day36 assignment

--- js/Assignment/multi_thread/multiThread.js
+++ js/Assignment/multi_thread/multiThread.js
@@ -1,19 +1,15 @@
 var worker;
 function displayTime() {
 
-    if (typeof(worker) == "undefined") {
-        worker = new Worker("worker.js");
-    }
+    worker = new Worker("worker.js");
     
     worker.onmessage = function(e) {
         console.log(e.data);
     }
-    
-    var date = new Date();
-    worker.postMessage(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
-
 }
 
 function displayHello() {
+    if(worker == 'undefined') 
+        worker.terminate();
     console.log('HELLO!!!');
 }
--- js/Assignment/multi_thread/worker.js
+++ js/Assignment/multi_thread/worker.js
@@ -1,4 +1,10 @@
-this.onmessage = function(e) {
-    this.postMessage(e.data);
-}
+// this.onmessage = function(e) {
+//     // this.postMessage(e.data);
+//     console.log(e.data);
+// }
 
+while(true) {
+    const date = new Date();
+    this.postMessage(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());   
+        
+}
