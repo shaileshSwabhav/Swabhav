e2e12aa4bebbb6c6798c0904f9af050213ef3696
shaileshhb <shaileshb.0720@gmail.com>
2020-08-20 16:16:13 +0530

    day38

--- jQuery/Assignment/todo/js/todo.js
+++ jQuery/Assignment/todo/js/todo.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    const taskNumber = localStorage.length;
+    var undoneList = [];
+
+    var taskNumber = localStorage.length;
     $('body').append('<div id="undone">');
     $('body').append('<div id="done">');
     $('#undone').append('<p id="undonePara">');
@@ -10,60 +12,61 @@ $(document).ready(function() {
     // task from localstorage
     for(var key in window.localStorage) {
         if(window.localStorage.hasOwnProperty(key)) {
-            console.log(localStorage.getItem(key));
-
-            $('#undonePara').append('<br>')
-            $('#undonePara').append("<input type='checkbox' class='" + key + "' value= '"+localStorage.getItem(key)+"'>");
-            $("#undonePara").append('<label id="'+ key +'">');
-
-            $("#"+key).text(localStorage.getItem(key));
+            addTask(event, key, true);
 
             //checkbox event
-            $('.'+key).on('change', function(event) {
-                checkboxEvent(event);
+            $('.'+key).on('click', function(event) {
+                removeTask(event);
             });
         }
     }
 
     // new task   
     $('#addTask').on('click', function() {
-        let task = $("#task").val();
         let classname = 'Task' + ++taskNumber;
-
-        $('#undonePara').append('<br>')
-        $('#undonePara').append("<input type='checkbox' class='" + classname + "' value= '"+task+"'>");
-        $("#undonePara").append('<label id="'+ classname +'">');
-
-        localStorage.setItem(classname, task);
-
-        $("#"+classname).text($("#task").val());
+        addTask(event, classname, false);
 
         //checkbox event
-        $('.'+classname).on('change', function(event) { 
-            checkboxEvent(event);
+        $('.'+classname).on('click', function(event) { 
+            removeTask(event);
         });
     });
 
-    function checkboxEvent(event) {
-        
-        //current date
-        var date = new Date();
-        var currentDate = (date.getDate() + "/"
-        + (date.getMonth()+1)  + "/" 
-        + date.getFullYear() + " @ "  
-        + date.getHours() + ":"  
-        + date.getMinutes() + ":" 
-        + date.getSeconds());
 
-        var taskName = localStorage.getItem($(event.target).attr('class')) + currentDate;
-        $('#donePara').text('Completed Task:');
-        $('#done').append('<p id="' + $(event.target).attr('class') + '">' + taskName +'</p>');
-        
+    function addTask(event, classname, isFromLocalstorage) {
+
+        $('#undonePara').append('<br>');
+        $("#undonePara").append('<label id="'+ classname +'">');
+        $('#undonePara').append("<input type='button' class='" + classname + "' value= Remove Task>");
+
+        displayTask(classname, isFromLocalstorage);
+    }
+
+    function removeTask(event) {
+ 
         //remove from undone list
         $("." + $(event.target).attr('class')).remove();
         $("#" + $(event.target).attr('class')).remove();
 
+        undoneList.splice(undoneList.indexOf(localStorage.getItem($(event.target).attr('class'))), 1);
+        console.log(undoneList);
         localStorage.removeItem($(event.target).attr('class'));
+    }
 
+    function displayTask(classname, isFromLocalstorage) {
+
+        var date = new Date();
+        var currentDate = (date.getFullYear() + "-"
+        + (date.getMonth()+1)  + "-" 
+        + date.getDate());
+
+        if(isFromLocalstorage == false) {
+            undoneList.push(($("#task").val()));
+            localStorage.setItem(classname, ($("#task").val()));
+            $("#"+classname).text($("#task").val() + " " + moment().fromNow());    
+        } else {
+            undoneList.push(localStorage.getItem(classname));
+            $("#"+classname).text(localStorage.getItem(classname) + " " + moment().fromNow());
+        }
     }
 });
