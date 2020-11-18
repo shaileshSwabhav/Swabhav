1c1c8800dfa6bdde62aa2b293701502c46b6f690
shaileshhb <shaileshb.07202gmail.com>
2020-11-18 18:44:14 +0530

    commit-1

--- Acme-company-modular-app/src/app/product/product.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-
-import { ProductListComponent } from "./product-list/product-list.component";
-import { ProductDetailsComponent } from './product-details/product-details.component';
-
-export const productRoutesArgs: Routes = [
-    { path: "productList", component: ProductListComponent },
-    { path: "productDetails", component: ProductDetailsComponent }
-]
